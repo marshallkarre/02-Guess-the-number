dfb3d050c6e8b0f6dbd6e3b29262ae769dbabfc3
ILS-Z399 <30897280+ILS-Z399@users.noreply.github.com>
2017-08-31 16:02:41 -0400

    Adds my solution to the assignment

--- 02 guess.py
+++ 02 guess.py
@@ -4,28 +4,35 @@ import sys
 import random
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
+guessesTaken = 0
 
-#generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
+print('Hello! What is your name?')
+myName = input()
+
 number = random.randint(1,20)
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
-
-
-#ask the user for a response and store it in the variable [guess]
-guess = input()
-
-
-
-#a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
-try:
-	#convert the guess to an integer
-	guess = int(guess)
-
-
-	#check if the guess is less than the random number
-	if guess < number:
-		print('Too low!')
-
-
-except ValueError:
-	print('Please enter a whole number.')
-
+for guessesTaken in range(6):
+	print('Take a guess')
+	guess = input()
+	try:
+		guess = int(guess)
+		
+		if guess < number:
+			print('Your guess is too low.')
+		
+		if guess > number:
+			print('Your guess is too high.')
+		
+		if guess == number:
+			break
+	except ValueError:
+		print('Please enter a number.')
+		
+if guess == number:
+	guessesTaken = str(guessesTaken + 1)
+	print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+	
+if guess != number:
+	number = str(number)
+	print('Nope. The number I was thinking of was ' + number + '.')
