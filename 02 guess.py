eebc1f4fa5787a1fbbfdf38b040b35cb93d0165e
marshallkarre <31518975+marshallkarre@users.noreply.github.com>
2017-09-13 10:47:56 -0400

    adds guessesMade, maxGuess, and numRange, deletes generate...and number, ads number and print, adds for guessesMade, guess, and try, deletes ask...and guess, adds guess=int, adss if guess and print too low, deletes #a..., # convert..., and guess=int, adds if guess and print to high, if guess= number and break, deletes #check..., if guess, and print too low, adds except error and print, deletes except error and print, adds if guess=number, guessesMade,print, if guess!=number, number, and print

--- 02 guess.py
+++ 02 guess.py
@@ -4,28 +4,39 @@ import sys
 import random
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
+guessesMade = 0
+maxGuesses = 5
+numRange = 15
 
-#generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
-number = random.randint(1,20)
+insertname = input('Karre ')
 
+number = random.randint(1,numRange)
+print('Hello, ' + insertname + '! I am thinking of a number between 1 and ' + str(numRange) + '.')
 
+for guessesMade in range(maxGuesses):
+	guess = input ('Can you guess what it is?')
+	try:
 
-#ask the user for a response and store it in the variable [guess]
-guess = input()
-
-
-
-#a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
-try:
-	#convert the guess to an integer
-	guess = int(guess)
-
-
-	#check if the guess is less than the random number
-	if guess < number:
-		print('Too low!')
+		guess = int(guess)
 
 
-except ValueError:
-	print('Please enter a whole number.')
+		if guess < number:
+			print('Too low!')
 
+		if guess > number:
+			print('Too high!')
+
+		if guess == number:
+			break
+
+
+	except ValueError:
+		print('Please enter a whole number.')
+
+if guess == number:
+	guessesMade = str(guessesMade + 1)
+	print('Excellent work, ' + insertname + '! You read my mind in ' + guessesMade + 'guesses!')
+
+if guess != number:
+	number = str(number)
+	print('Sorry. The number I was looking for was ' + number + '.')
